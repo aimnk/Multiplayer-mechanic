db960899770e2b9d503163b7cb687c296f0e3eb9
aimnk <aimonka@bk.ru>
2022-10-18 22:01:21 +0700

    добавление скрипта для блокировки курсора

---
+++ Assets/GameResources/Scripts/UI/CursorLocker.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameResources.Scripts.UI
+{
+    /// <summary>
+    /// Локер курсора
+    /// </summary>
+    public class CursorLocker : MonoBehaviour
+    {
+        private PlayerInput playerInput;
+
+        private void Awake() => playerInput = new PlayerInput();
+
+        private void OnEnable()
+        {
+            playerInput.UI.EscapeButton.performed += OnEscapeButton;
+            playerInput.UI.ClickMouse.performed += OnClickMouse;
+            playerInput.Enable();
+            SetLockCursor(false);
+        }
+
+        private void OnDisable()
+        {
+            playerInput.UI.EscapeButton.performed -= OnEscapeButton;
+            playerInput.UI.ClickMouse.performed -= OnClickMouse;
+        }
+
+        private void OnClickMouse(InputAction.CallbackContext context) => SetLockCursor(false);
+
+        private void OnEscapeButton(InputAction.CallbackContext context) => SetLockCursor(true);
+
+        private void SetLockCursor(bool state)
+        {
+            Cursor.visible = state;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
